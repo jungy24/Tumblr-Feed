bcb212cca1e0c9ab0eff046264e7b453189a2331
Jungyoon Yu <yu612@purdue.edu>
2017-02-10 00:45:14 -0500

    Added refresh to both ends

---
+++ Tumblr Feed/InfiniteScrollView.swift
@@ -0,0 +1,21 @@
+//
+//  InfiniteScrollView.swift
+//  Tumblr Feed
+//
+//  Created by Jungyoon Yu on 2/9/17.
+//  Copyright © 2017 Jungyoon Yu. All rights reserved.
+//
+
+import UIKit
+
+class InfiniteScrollView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
